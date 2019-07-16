93ff5664edd8d1eaaf1d0b2c221a2c24ed941922
BLOT Martial <martial.blot@outlook.fr>
2019-07-16 17:05:52 +0200

    supprimer messages OK

--- wc-UI/src/app/infos/infos.component.ts
+++ wc-UI/src/app/infos/infos.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InfosService } from '../services/infos.service';
 import { Infos } from '../models/infos.model';
+import { MessagesService } from '../services/messages.service';
+import { Messages } from '../models/messages.model';
 
 @Component({
   selector: 'app-infos',
@@ -12,10 +14,13 @@ export class InfosComponent implements OnInit {
 
   public id: number;
   public infos;
+  public messages;
+  public openMailer = true;
 
   constructor(
     private route: ActivatedRoute,
-    private service: InfosService
+    private service: InfosService,
+    private messageService: MessagesService,
     ) { }
 
   ngOnInit() {
@@ -25,11 +30,24 @@ export class InfosComponent implements OnInit {
     this.service.getInfos(this.id).subscribe((infos: Infos) =>{
       this.infos = infos;
     })
+    this.messageService.getMessages(this.id).subscribe((message: Messages) =>{
+      this.messages = message;
+    })
   }
 
   deleteInfos(id, index){
     this.service.deleteInfos(id).subscribe((infos: Infos) => {
       this.infos.splice(index, 1)
+    })
+  }
+
+  showMessages(){
+    this.openMailer = !this.openMailer;
+  }
+
+  deleteMessage(id, index){
+    this.messageService.deleteMessage(id).subscribe((message: Messages) =>{
+      this.messages.splice(index, 1);
     })
   }
 }
--- wc-UI/src/app/services/messages.service.ts
+++ wc-UI/src/app/services/messages.service.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Messages } from '../models/messages.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,21 @@ export class MessagesService {
 
   public sendMessage(messagesForm: Messages): Observable <Messages>{
     return this.http.post<Messages>(`${this.api}/messages`, messagesForm);
+  }
+
+  public getMessages(id: number): Observable<Messages>{
+    return this.http.get(`${this.api}/circus/${id}/messages`).pipe(
+      map((messages: any) => { return messages as Messages
+      })
+      )
+  };
+
+  public deleteMessage(id: number): Observable<Messages> {
+    return this.http.delete(`${this.api}/messages/${id}`).pipe(
+      map((messages: any) => {
+        return  messages as Messages;
+      }),
+    );
   }
 
 }
