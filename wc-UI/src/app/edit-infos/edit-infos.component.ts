9a733816d0e6fc5354ec53454fc78460532f84c7
BLOT Martial <martial.blot@outlook.fr>
2019-07-16 15:45:55 +0200

    edit infos

--- wc-UI/src/app/edit-infos/edit-infos.component.ts
+++ wc-UI/src/app/edit-infos/edit-infos.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CircusService } from '../services/circus.service';
 import { InfosService } from '../services/infos.service';
-import { Circus } from '../models/circus.model';
 import { Infos } from '../models/infos.model';
 
 @Component({
@@ -13,12 +11,10 @@ import { Infos } from '../models/infos.model';
 export class EditInfosComponent implements OnInit {
 
   public id: number;
-  public circus;
   public infos;
 
   constructor(
     private route: ActivatedRoute,
-    private service: CircusService,
     private infoService: InfosService,
     private router: Router,
   ) { }
@@ -29,16 +25,12 @@ export class EditInfosComponent implements OnInit {
       this.infoService.getOneInfos(this.id).subscribe((infos: Infos) => {
         this.infos = infos[0];
       })
-      this.service.getOneCircus(this.infos.circus_id).subscribe((circus: Circus) => {
-        this.circus = circus[0];
-      })
     })
-
   }
 
   editInfos(id, infos){
     this.infoService.editInfos(id, infos.value).subscribe((infos: Infos) => {
-      // this.router.navigateByUrl("['circus',circus.id,'infos']");
+      this.router.navigateByUrl("");
     })
   }
 
